98f99165070bdddcb150dbd256af0a7c6d717cb6
seaswalker <xsdwem7@hotmail.com>
2018-08-24 09:28:03 +0800

    hello world

---
+++ chapter1/echo.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	stupidLoop()
+	betterLoop()
+	noLoop()
+}
+
+func stupidLoop() {
+	var s, seq string
+	for i := 1; i < len(os.Args); i++ {
+		s += seq + os.Args[i]
+		seq = " "
+	}
+	fmt.Println(s)
+}
+
+func betterLoop() {
+	s, seq := "", ""
+
+	for _, arg := range os.Args[1:] {
+		s += seq + arg
+		seq = " "
+	}
+
+	fmt.Println(s)
+}
+
+func noLoop() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
